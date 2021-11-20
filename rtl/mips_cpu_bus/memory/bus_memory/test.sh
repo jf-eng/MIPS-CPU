4e0f9ae279eff4846b285ff20693797dbdaba2a0
jf-eng <engjianfu@gmail.com>
2021-11-20 22:17:38 +0000

    redefined file structure

--- rtl/mips_cpu_bus/memory/bus_memory/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-iverilog -Wall -g 2012 -s bus_memory_tb -o bus_memory_tb *.v
-./bus_memory_tb
--- rtl/mips_cpu_bus/memory/external_ram/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-iverilog -Wall -g 2012 -s external_ram_tb -o external_ram_tb *.v
-./external_ram_tb
