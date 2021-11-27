b5ec6a189ef4c5200b1ba440d16106154a664cb9
Derek Lai <derek.lai002@gmail.com>
2021-11-27 14:22:37 +0000

    problem with div in assembler

--- test/harvard/div/div_0.s
+++ test/harvard/div/div_0.s
@@ -1,11 +1,12 @@
 .config
 	ARCH h
-	ASSERT 
+	ASSERT 0x5
 
 .text
 	ADDIU $1 $0 #20
 	ADDIU $2 $0 #4
 	DIV $1 $2
-	MFLO
+	MFLO $2
+	JR $0
 
 .data
--- test/harvard/run_harvard_instr.sh
+++ test/harvard/run_harvard_instr.sh
@@ -1,25 +1,66 @@
 #!/bin/bash
 
 INSTR="$1"
-
+RED='\033[0;31m'
+NC='\033[0m'
 ASM_FILES=$(ls ./$INSTR/*.s)
+
+
+# # Options
+# while getopts "vkf:" options; do
+
+# 	case "${options}" in
+# 		v)
+# 			VERBOSE=1
+# 			echo "SET verbose"
+# 			;;
+# 		k)
+# 			KEEP_FILES=1
+# 			echo "SET keep"
+# 			;;
+# 		f)
+# 			FILENAME=""
+# 			echo "SET file TO ${OPTARG}"
+# 	esac
+
+# done
+
+# if [[ -z FILENAME ]]; then
+# 	echo "Please provide file name -f <name>"
+# 	exit 2
+# fi
 
 for file in $ASM_FILES; do
 
 	# Assemble
-	./assemble $file &>/dev/null
+	./assemble $file &> assem_err.log
+
+	if [[ $? -ne 0 ]]; then
+		echo -e "${RED}ERROR assembling ${file}${NC}"
+		cat assem_err.log
+		rm assem_err.log
+		exit 2
+	fi
 
 	# Strip name
 
 	STRIPPED=$(basename $file .s)
 
 	# Compile entire CPU w/ testbench & instruction to test
-	iverilog -Wall -g 2012 -s $STRIPPED -o ./$INSTR/$STRIPPED mips_cpu_harvard_tb.v ./$INSTR/$STRIPPED.v $(find ./../../rtl/harvard -name "*.v") &>/dev/null
+	iverilog -Wall -g 2012 -s $STRIPPED -o ./$INSTR/$STRIPPED mips_cpu_harvard_tb.v ./$INSTR/$STRIPPED.v $(find ./../../rtl/harvard -name "*.v") &> comp_err.log
+
+	if [[ $? -ne 0 ]]; then
+		echo -e "${RED}ERROR compiling ${file}${NC}"
+		cat comp_err.log
+		rm comp_err.log
+		exit 2
+	fi
 
 	# Run (Cannot run from here, must run in the folder itself for some reason)
 	cd $INSTR
 
-	./$STRIPPED &>/dev/null
+	./$STRIPPED
+	#  &>/dev/null
 
 	# Check Pass/Fail
 	if [[ $? -ne 0 ]]; then
@@ -29,7 +70,7 @@ for file in $ASM_FILES; do
 	fi
 
 	# remove the files
-	rm -rf $STRIPPED $STRIPPED.ram $STRIPPED.rom $STRIPPED.v
+	# rm -rf $STRIPPED $STRIPPED.ram $STRIPPED.rom $STRIPPED.v
 
 	cd ..
 
