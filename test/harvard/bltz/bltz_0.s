ceeead63b8d4755df69d1b0a09078ba2c95014b2
Derek Lai <derek.lai002@gmail.com>
2021-11-29 23:26:36 +0000

    all currently merged cases passing

--- test/harvard/bltz/bltz_0.s
+++ test/harvard/bltz/bltz_0.s
@@ -1,16 +1,17 @@
+// BLTZ branch false test
+
 .config
-        ARCH h
-        ASSERT 3
+	ARCH h
+	ASSERT 2
+
 .text
-        ADDIU $1 $0 #1
-        ADDIU $2 $0 #4
-        BLTZ $2 jump
-        SUBU $2 $2 $1
-        jump:
-        SUBU $2 $2 $1
-        end:
-        JR $0
+		ADDIU $1 $0 #1 // $1 = 1
+		ADDIU $2 $0 #4 // $2 = 4
+		BLTZ $2 jump   // no branch
+		SUBU $2 $2 $1  // $2 = 3
+	jump:
+		SUBU $2 $2 $1  // $2 = 2
+	end:
+		JR $0
+
 .data
-
-
-
