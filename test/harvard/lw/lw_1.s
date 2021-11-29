8dc796493e536cd4cc1f7747844a2bc911e1d96d
Derek Lai <derek.lai002@gmail.com>
2021-11-29 22:32:00 +0000

    lw 1

--- test/harvard/lw/lw_1.s
+++ test/harvard/lw/lw_1.s
@@ -1,13 +1,11 @@
-// hi this is a commnet h'9 200 230 s
-
 .config
 	ARCH h
-	ASSERT 32'hD2F1ACB0
+	ASSERT 32'hFFFFFFFF
 
 .text
-	LW $2 0($0)
-	// XORI $2 $2 #0x25FB
 	JR $0
+	LW $2 4($0)
 
 .data
 	#0xD2F1ACB0
+	#0xFFFFFFFF
