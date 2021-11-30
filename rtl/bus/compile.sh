908ecf915819954a3f21d42e34383d6a349aa097
Derek Lai <derek.lai002@gmail.com>
2021-11-30 16:29:15 +0000

    cleaned up folders and tests

--- rtl/bus/compile.sh
+++ rtl/bus/compile.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-iverilog -Wall -g 2012 -o mips_cpu_bus_tb -s mips_cpu_bus_tb $(find . -name "*.v")
+iverilog -Wall -g 2012 -o mips_cpu_bus -s mips_cpu_bus $(find . -name "*.v")
 
-./mips_cpu_bus_tb
-
-rm mips_cpu_bus_tb
+rm mips_cpu_bus
--- rtl/harvard/compile.sh
+++ rtl/harvard/compile.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-iverilog -Wall -g 2012 -o mips_cpu_harvard_tb -s mips_cpu_harvard_tb $(find . -name "*.v")
+iverilog -Wall -g 2012 -o mips_cpu_harvard -s mips_cpu_harvard $(find . -name "*.v")
 
-./mips_cpu_harvard_tb
-
-rm mips_cpu_harvard_tb
+rm mips_cpu_harvard
